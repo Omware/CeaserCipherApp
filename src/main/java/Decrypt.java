fb7fc0437a81c630431768a73f1c192284e45283
Keith Omware <keithomware@gmail.com>
2019-11-10 22:52:33 +0300

    Add logic to Decrypt.java class

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,3 +1,19 @@
 public class Decrypt {
+    private int userKey;
+    private String userInputText;
+    public Decrypt(String InputText, int key) {
+        userKey = key;
+        userInputText = InputText;
+    }
+    public String getUserInputText() {
+        return userInputText;
+    }
+    public int getUserKey() {
+
+        return userKey;
+    }
+    public String getDecription() {
+
+    }
 
 }
--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -1,5 +1,25 @@
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class DecryptTest {
+    @Test
+    public void getUserInput_string(){
+        Decrypt testEncrypting = new Decrypt("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD", 1);
+        String expectedOutput = "QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD";
+        assertTrue(expectedOutput==testEncrypting.getUserInputText());
+    }
+    @Test
+    public void getUserKey_Integer(){
+        Decrypt testEncrypting = new Decrypt("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD", 1);
+        int expectedOutput = 1;
+        assertTrue(expectedOutput==testEncrypting.getUserKey());
+    }
+    @Test
+    public void decryptuserInputString_string(){
+        Decrypt testEncrypting = new Decrypt("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD", 1);
+        String expectedOuput = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG";
+        assertEquals(expectedOuput,testEncrypting.getDecription());
+    }
+
 
 }
